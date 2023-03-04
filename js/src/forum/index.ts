2b52c1f9a17392614963ed80142aa8d6adf31391
William <nearata@protonmail.com>
2023-03-04 18:03:15 +0100

    fix: wait for the library to be fully loaded
    * avoids potential errors

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -7,15 +7,17 @@ import app from "flarum/forum/app";
  * ref: https://github.com/flarum/pusher/blob/v1.6.1/js/src/forum/index.ts
  */
 app.initializers.add("nearata-websocket", () => {
-  app.websocket = import(
-    "//cdnjs.cloudflare.com/ajax/libs/centrifuge/3.1.0/centrifuge.min.js" /* webpackIgnore: true, webpackPrefetch: true */
-  ).then(() => {
+  app.websocket = (async () => {
+    await import(
+      "//cdnjs.cloudflare.com/ajax/libs/centrifuge/3.1.0/centrifuge.min.js" /* webpackIgnore: true, webpackPrefetch: true */
+    );
+
     const websocket = new Websocket();
 
     websocket.init();
 
     return websocket;
-  });
+  })();
 
   discussions();
   notifications();
